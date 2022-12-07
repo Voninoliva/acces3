799d9756477a02d33b8e70a62968c0c43180c656
Voninoliva <voninolivam@gmail.com>
2022-12-07 16:14:36 +0300

    valider

--- Myserveur.java
+++ Myserveur.java
@@ -3,8 +3,6 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Vector;
-
-import serveur.Rescreen;
 public class Myserveur{
     public static void main(String[] args) throws Exception{
         ServerSocket serverSocket=new ServerSocket(3000);
